0c953e2e7ff24e22d817a359d9438a6f4f404b15
ctingBiz2020 <chong.leong.ting@gmail.com>
2020-09-09 14:46:32 -0500

    create the board and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BoardgameComponent } from './boardgame/boardgame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardgameComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/boardgame/boardgame.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardgameComponent } from './boardgame.component';
+
+describe('BoardgameComponent', () => {
+  let component: BoardgameComponent;
+  let fixture: ComponentFixture<BoardgameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BoardgameComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardgameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
