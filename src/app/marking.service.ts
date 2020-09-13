0e325594fc4a809048e0f77018655b664e3f50e4
ctingBiz2020 <chong.leong.ting@gmail.com>
2020-09-12 20:29:33 -0500

    check xmarker image

--- src/app/marking.service.ts
+++ src/app/marking.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +8,7 @@ export class MarkingService {
   // Create a new element
   addNewElement(){
     let xMarker = document.createElement("img");
-    xMarker.src = "assets/xMarker.png";
+    xMarker.src = "xMarker.png";
     xMarker.width = 50;
     xMarker.height = 50;
     xMarker.alt = "image.";
